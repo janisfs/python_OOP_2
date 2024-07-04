dd9a75f7223ce4f9be4bac72820e60ec1b1c2490
janisfs <janisfs@mail.ru>
2024-07-04 22:01:32 +0300

    Create super().py

---
+++ super().py
@@ -0,0 +1,20 @@
+class A:
+    def __init__(self):
+        print("Class A")
+
+class B(A):
+    def __init__(self):
+        super().__init__()
+        print("Class B")
+
+class C(A):
+    def __init__(self):
+        super().__init__()
+        print("Class C")
+
+class D(B, C):
+    def __init__(self):
+        super().__init__()
+        print("Class D")
+
+d = D()
